d711bc9224dcb7f13e716b063cd1be341ebe05a7
SpikeGD <spikegdofficial@gmail.com>
2020-06-15 08:46:36 -0700

    Formatting

--- eBayShippingCalculator.js
+++ eBayShippingCalculator.js
@@ -23,16 +23,16 @@
   // Sometimes the lists are different. Beats me as to why
   var list = $('#ListViewInner').length > 0 ?  $('#ListViewInner'):$('.srp-results')
   function getShipping(val) {
-		var shipping = $(val).find('.lvshipping').text().trim() || $(val).find('.s-item__shipping').text().trim()
+	var shipping = $(val).find('.lvshipping').text().trim() || $(val).find('.s-item__shipping').text().trim()
   	return shipping.replace(/^\D+/g, '').replace(',', '')
-	}
+  }
   
   function getPrice(val) {
   	var price = $(val).find('.lvprice').text().trim() || $(val).find('.s-item__price').text().trim()
   	return price.replace(/^\D+/g, '').replace(',', '')
   }
 
-	list.children('li').each((i, val) => {
+  list.children('li').each((i, val) => {
     // Get the objects, there are (from what I've seen) two different possibilities. Maybe there are more, idk
     var shipObj = $(val).find('.lvshipping').length > 0 ? $(val).find('.lvshipping'):$(val).find('.s-item__shipping')
     var priceObj = $(val).find('.lvprice').length > 0 ? $(val).find('.lvprice'):$(val).find('.s-item__price')
